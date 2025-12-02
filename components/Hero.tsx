e46d12304d44859722d5045a264436a07f9a13b8
1t0t0 <totophandolack915@gmail.com>
2025-12-02 19:29:08 +0700

    Hero Video Animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,10 +2,15 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const Hero = () => {
+    const videoRef = useRef<HTMLVideoElement>(null)
+
+    const isMobile = useMediaQuery({ maxWidth: 767 })
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' });
@@ -18,7 +23,6 @@ const Hero = () => {
             duration: 1.8,
             ease: 'expo.out',
             stagger: 0.05,
-            // delay: 1,
         });
 
         gsap.from(paragraphSplit.lines, {
@@ -41,44 +45,76 @@ const Hero = () => {
             .to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0)
 
+        const startValue = isMobile ? 'top 50%' : 'center 60%'
+        const endValue = isMobile ? '120% top' : 'bottom top'
 
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+
+        if (videoRef.current) {
+            videoRef.current.onloadedmetadata = () => {
+                tl.to(videoRef.current!, {
+                    currentTime: videoRef.current!.duration,
+                });
+            };
+        }
 
     }, [])
 
     return (
-        <section id='hero' className='noisy'>
-            <h1 className='title'>SOMSA</h1>
 
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-                src="/images/hero-left-leaf.png"
-                alt="left-leaf"
-                className="left-leaf"
-            />
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/images/hero-right-leaf.png"
-                alt="right-leaf"
-                className="right-leaf"
-            />
+        <>
+            <section id='hero' className='noisy'>
+                <h1 className='title'>SOMSA</h1>
 
-            <div className="body">
-                <div className="content">
-                    <div className="space-y-5 hidden md:block">
-                        <p>Cool. Crisp. Classic</p>
-                        <p className="subtitle">
-                            Sip the Spirit <br /> of Summer
-                        </p>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    src="/images/hero-left-leaf.png"
+                    alt="left-leaf"
+                    className="left-leaf"
+                />
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src="/images/hero-right-leaf.png"
+                    alt="right-leaf"
+                    className="right-leaf"
+                />
+
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Classic</p>
+                            <p className="subtitle">
+                                Sip the Spirit <br /> of Summer
+                            </p>
+                        </div>
+
+                        <div className="view-cocktails">
+                            <p className="subtitle">Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable. </p>
+                            <a href="#cocktails">View Cocktails</a>
+                        </div>
+
                     </div>
-
-                    <div className="view-cocktails">
-                        <p className="subtitle">Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable. </p>
-                        <a href="#cocktails">View Cocktails</a>
-                    </div>
-
                 </div>
-            </div>
 
-        </section >
+            </section >
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    muted
+                    playsInline
+                    preload="auto"
+                    src="/videos/output.mp4"
+                />
+            </div>
+        </>
     )
 }
 
